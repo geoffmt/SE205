3a215548e615fa92fba11514b0c6fc77ad40bd5a
Geoffroy MATEU <geoffroy.mateu@telecom-paristech.fr>
2020-02-12 22:18:59 +0100

    il faut finir le put regarder le screenshot sur mon tel

--- TP3/cond_protected_buffer.c
+++ TP3/cond_protected_buffer.c
@@ -6,12 +6,15 @@
 #include "protected_buffer.h"
 #include "utils.h"
 
-// Initialise the protected buffer structure above. 
+// Initialise the protected buffer structure above.
 protected_buffer_t * cond_protected_buffer_init(int length) {
   protected_buffer_t * b;
   b = (protected_buffer_t *)malloc(sizeof(protected_buffer_t));
   b->buffer = circular_buffer_init(length);
   // Initialize the synchronization components
+  pthread_mutex_init(&(b->mutex), NULL);
+  pthread_cond_init(&(b->condEmpty), NULL);
+  pthread_cond_init(&(b->condFull), NULL);
   return b;
 }
 
@@ -19,20 +22,23 @@ protected_buffer_t * cond_protected_buffer_init(int length) {
 // not possible immedidately, the method call blocks until it is.
 void * cond_protected_buffer_get(protected_buffer_t * b){
   void * d;
-  
+
   // Enter mutual exclusion
-  
+  pthread_mutex_lock(&(b->mutex));
   // Wait until there is a full slot to get data from the unprotected
   // circular buffer (circular_buffer_get).
-  
+  while ((d = circular_buffer_get(b->buffer)) == NULL){
+    pthread_cond_wait(&(b->condFull), &(b->mutex));
+  }
   // Signal or broadcast that an empty slot is available in the
   // unprotected circular buffer (if needed)
+  pthread_cond_broadcast(&(b->condEmpty));
 
   d = circular_buffer_get(b->buffer);
   print_task_activity ("get", d);
 
   // Leave mutual exclusion
-  
+  pthread_mutex_unlock(&(b->mutex));
   return d;
 }
 
@@ -41,10 +47,10 @@ void * cond_protected_buffer_get(protected_buffer_t * b){
 void cond_protected_buffer_put(protected_buffer_t * b, void * d){
 
   // Enter mutual exclusion
-  
+
   // Wait until there is an empty slot to put data in the unprotected
   // circular buffer (circular_buffer_put).
-  
+
   // Signal or broadcast that a full slot is available in the
   // unprotected circular buffer (if needed)
 
@@ -58,14 +64,14 @@ void cond_protected_buffer_put(protected_buffer_t * b, void * d){
 // possible immedidately, return NULL. Otherwise, return the element.
 void * cond_protected_buffer_remove(protected_buffer_t * b){
   void * d;
-  
+
 
   // Signal or broadcast that an empty slot is available in the
   // unprotected circular buffer (if needed)
 
   d = circular_buffer_get(b->buffer);
   print_task_activity ("remove", d);
-  
+
   return d;
 }
 
@@ -73,9 +79,9 @@ void * cond_protected_buffer_remove(protected_buffer_t * b){
 // not possible immedidately, return 0. Otherwise, return 1.
 int cond_protected_buffer_add(protected_buffer_t * b, void * d){
   int done;
-  
+
   // Enter mutual exclusion
-  
+
   // Signal or broadcast that a full slot is available in the
   // unprotected circular buffer (if needed)
 
@@ -94,16 +100,16 @@ int cond_protected_buffer_add(protected_buffer_t * b, void * d){
 void * cond_protected_buffer_poll(protected_buffer_t * b, struct timespec *abstime){
   void * d = NULL;
   int    rc = 0;
-  
+
   // Enter mutual exclusio
-  
+
   // Wait until there is an empty slot to put data in the unprotected
   // circular buffer (circular_buffer_put) but waits no longer than
   // the given timeout.
-  
+
   // Signal or broadcast that a full slot is available in the
   // unprotected circular buffer (if needed)
-  
+
   d = circular_buffer_get(b->buffer);
   print_task_activity ("poll", d);
 
@@ -119,20 +125,20 @@ void * cond_protected_buffer_poll(protected_buffer_t * b, struct timespec *absti
 int cond_protected_buffer_offer(protected_buffer_t * b, void * d, struct timespec * abstime){
   int rc = 0;
   int done = 0;
-  
+
   // Enter mutual exclusion
-  
+
   // Signal or broadcast that a full slot is available in the
   // unprotected circular buffer (if needed) but waits no longer than
   // the given timeout.
 
   // Signal or broadcast that a full slot is available in the
   // unprotected circular buffer (if needed)
-  
+
   done = circular_buffer_put(b->buffer, d);
   if (!done) d = NULL;
   print_task_activity ("offer", d);
-    
+
   // Leave mutual exclusion
   return done;
 }
--- TP3/protected_buffer.h
+++ TP3/protected_buffer.h
@@ -9,6 +9,9 @@
 typedef struct {
   long                sem_impl;
   circular_buffer_t * buffer;
+  pthread_mutex_t mutex;
+  pthread_cond_t condEmpty;
+  pthread_cond_t condFull;
 } protected_buffer_t;
 
 // Initialise the protected buffer structure above. sem_impl specifies
